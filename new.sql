fb3276cba315f03e1ce52566171ace66b3a4d0f6
JohnathanEinstein <yonatanawlachew1@gmail.com>
2025-03-07 21:30:57 +0100

    Update new.sql

--- new.sql
+++ new.sql
@@ -1,4 +1,4 @@
 
-SELECT Order_Priority, Ship_Mode  ,count(Ship_Mode)
-FROM platinum-goods-452923-t1.SuperSell.Cleaned_data
-group by Order_Priority,Ship_Mode
+SELECT Order_Priority, Ship_Mod ,count(Ship_Mode)
+FROM Cleaned_data
+group by Order_Priority,Ship_Mode;
